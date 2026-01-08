c92b14c5c8df90bad860a9d3812a69f865724422
Pablo <jlobagon@myuax.com>
2026-01-08 23:54:54 +0100

    remove unused loader fade-out logic from App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,6 @@ import BootScreen from "./components/os/BootScreen.jsx";
 
 function App() {
     const [showLoader, setShowLoader] = useState(true);
-    const [fadeOut, setFadeOut] = useState(false);
-    {/*
-    useEffect(() => {
-        const boottime = 3000;
-        const fadeTime = 1000;
-        if(!loading) {
-            setFadeOut(true);
-
-            const timer = setTimeout(() => {
-                setShowLoader(false);
-            }, 4000);
-
-            return () => clearTimeout(timer);
-        }
-    }, [loading]);
-    */}
-
 
     return (
         <>
