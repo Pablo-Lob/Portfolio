842f654015b5462d93bffbbc549c25fae99c7a40
Pablo <jlobagon@myuax.com>
2026-01-10 10:18:15 +0100

    add clock functionality and workspace buttons to Desktop component

--- src/components/os/Desktop.jsx
+++ src/components/os/Desktop.jsx
@@ -1,7 +1,27 @@
+import {useState, useEffect} from 'react';
 import '../../styles/Desktop.css';
 import logo from '../../assets/images/kali_logo.png';
 
 function Desktop() {
+    const [date, setDate] = useState(new Date());
+
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+        return () => clearInterval(timer);
+    }, []);
+
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+
+    const formatAmPm = hour >= 12 ? 'PM' : 'AM'; // Formato AM/PM
+
+    const format12 = hour % 12 || 12; // Formato 12 horas
+
+    const timeString = `${format12}:${minutes.toString().padStart(2, '0')} ${formatAmPm}`; // Formato HH:MM AM/PM
+
     return (
         <div className="desktop-container">
             {/* Barra de tareas */}
@@ -9,15 +29,30 @@ function Desktop() {
 
                 <div className="taskbar-left">
                     <img className="logo-kali" src={logo} alt="Logo kali"/>
+
+                    <div className="separator"></div>
+
+                    <div className="terminal-icon">
+                        <span style={{fontWeight: 'bold', fontSize: '14px'}}>_&gt;</span>
+                    </div>
+
+                    <div className="separator"></div>
+
+                    <div className="workspaces">
+                        <div className="workspace-btn active">1</div>
+                        <div className="workspace-btn">2</div>
+                        <div className="workspace-btn">3</div>
+                        <div className="workspace-btn">4</div>
+                    </div>
+
                     <div className="separator"></div>
                 </div>
 
 
 
                 <div className="taskbar-right">
-
                     <div className="clock">
-                        12:35 PM
+                        {timeString}
                     </div>
 
                     <div className="network-icon">
@@ -34,9 +69,9 @@ function Desktop() {
 
                     <div className="separator"></div>
 
-                    <diva className="padlock-icon">
+                    <div className="padlock-icon">
                         🔒︎
-                    </diva>
+                    </div>
 
                     <div className="power-icon">
                         ⏻
